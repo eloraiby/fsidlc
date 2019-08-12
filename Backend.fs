23e5b2e1c763aa25f3f44dbd3670e0a68787bf49
Wael El Oraiby <wael.eloraiby@gmail.com>
2019-08-11 23:12:02 -0400

    Start with the backend

--- Backend.fs
+++ Backend.fs
@@ -1,1 +1,25 @@
 module Backend
+
+open Ast
+
+let reorder (m: Module) =
+
+    let decls   = m.decls
+
+    let refTys
+        = m.decls
+        |> List.filter (
+            function
+            | Decl.DeclObj _
+            | Decl.DeclInterface _ -> true
+            | _ -> false)
+    let vTys
+        = m.decls
+        |> List.filter (
+            function
+            | Decl.DeclObj _
+            | Decl.DeclInterface _ -> false
+            | _ -> true)
+
+
+    failwith "todo"
